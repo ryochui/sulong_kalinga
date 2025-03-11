6230fd61b181f4155bd24953a018e23df89820ba
ryochui <155883482+ryochui@users.noreply.github.com>
2025-03-12 03:22:36 +0800

    Migrations for municipalities and barangays
    Changelog:
    
    - Created migrations for municipalities table
    - Created migrations for barangays table

---
+++ database/migrations/2025_03_11_191644_create_municipalities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('municipalities', function (Blueprint $table) {
+            $table->increments('municipality_id');
+            $table->string('municipality_name', 100)->after('municipality_id');
+            $table->integer('province_id')->after('municipality_name');
+            $table->timestamps();
+
+             // Foreign Key Constraints
+             $table->foreign('province_id')->references('province_id')->on('provinces')->onDelete('cascade');
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('municipalities');
+    }
+};
