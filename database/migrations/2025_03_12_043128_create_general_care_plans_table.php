9c2297c8064635bf065768c22de684330ae5fd13
ryochui <155883482+ryochui@users.noreply.github.com>
2025-03-12 13:53:42 +0800

    Migrations for gcp and some related tables
    Changelog:
    - Updated ERD Page 3 and 4
    - Created Migrations for general_care_plans, care_needs, care_categories, health_histories and history_categories table
    - GCP still need to add emergency_plan column

---
+++ database/migrations/2025_03_12_043128_create_general_care_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('general_care_plans', function (Blueprint $table) {
+            $table->id('general_care_plan_id');
+            $table->unsignedBigInteger('beneficiary_id')->unique()->after('general_care_plan_id');
+            $table->unsignedBigInteger('care_worker_id')->after('beneficiary_id');
+            $table->timestamps();
+
+            // Foreign Key Constraints
+        $table->foreign('care_worker_id')->references('id')->on('cose_users')->onDelete('no action');
+        });
+
+        
+        
+        // Separate Migration after creation of Beneficiary Table:
+        // $table->foreign('beneficiary_id')->references('beneficiaries')->on('beneficiary_id')->onDelete('no action');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('general_care_plans');
+    }
+};
---
+++ database/migrations/2025_03_12_044837_create_care_needs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('care_needs', function (Blueprint $table) {
+            $table->id('care_need_id');
+            $table->unsignedBigInteger('general_care_plan_id')->after('care_need_id');
+            $table->integer('care_category_id')->after('general_care_plan_id');
+            $table->string('frequency', 100)->after('care_category_id');
+            $table->text('assistance_required')->after('frequency');
+            $table->timestamps();
+
+            // Foreign Key Constraints
+            $table->foreign('general_care_plan_id')->references('general_care_plan_id')->on('general_care_plans')->onDelete('cascade');
+            $table->foreign('care_category_id')->references('care_category_id')->on('care_categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('care_needs');
+    }
+};
