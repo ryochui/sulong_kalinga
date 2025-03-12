8387bc8332d7e3480a4081a7c3be3379760d43cf
ryochui <155883482+ryochui@users.noreply.github.com>
2025-03-12 23:05:40 +0800

    Migrations for Needed GCP Tables
    Changelog:
    - Create migrations for medications, mobility, cognitive_function, emotional_wellbeing, and care_worker_responsibilities table for gcp

---
+++ database/migrations/2025_03_12_143835_create_care_worker_responsibilities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('care_worker_responsibilities', function (Blueprint $table) {
+            $table->increments('cw_responsibility_id');
+            $table->integer('general_care_plan_id')->after('cw_responsibility_id');
+            $table->integer('care_worker_id')->after('general_care_plan_id');
+            $table->text('task_description')->after('care_worker_id');
+            $table->timestamps();
+
+            // Foreign Key Constraints
+            $table->foreign('general_care_plan_id')->references('general_care_plan_id')->on('general_care_plans')->onDelete('cascade');
+            $table->foreign('care_worker_id')->references('id')->on('cose_users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('care_worker_responsibilities');
+    }
+};
