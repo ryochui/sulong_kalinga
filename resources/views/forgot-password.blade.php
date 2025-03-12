34010bee3aef7d5dc55af0da060f22a9b956eaa5
centvincent <saguinsinvincent@gmail.com>
2025-03-13 05:10:15 +0800

    forgotpass

--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -3,15 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-
     @include('components.navbar')
-    
-    <div class="container text-center" id="login">
+    <div class="container text-center" id="forgot-password">
         <div class="row justify-content-center">
             <div class="col">
             
@@ -19,31 +17,25 @@
             <div class="col-md-6">
                 <div class="card shadow">
                     <div class="card-body">
-                        <h2 class="text-center">Login</h2>
+                        <h2 class="text-center">Forgot Password</h2>
                         <form action="#" method="POST">
                             <div class="mb-3">
                                 <label class="form-label">Email</label>
                                 <input type="email" class="form-control" placeholder="lastnameFirstname@example.com" required>
                             </div>
-                            <div class="mb-3">
-                                <label class="form-label">Password</label>
-                                <input type="password" class="form-control" placeholder="Enter your password" required>
-                            </div>
-                            <div class="mb-3 text-end">
-                                <a href="{{ route('forgotPass') }}">Forgot password?</a>
-                            </div>
-                            <button type="submit" class="btn btn-primary w-100" id="submitLogin">Login</button>
+                            <button type="submit" class="btn btn-primary w-100">Submit</button>
                         </form>
                     </div>
                 </div>
             </div>
             <div class="col">
-            
+
             </div>
         </div>
     </div>
-        
-
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view ('login');
 })->name('login');
+
+Route::get('/forgot-password', function () {
+    return view ('forgot-password');
+})->name('forgotPass');
