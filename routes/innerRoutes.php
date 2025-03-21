36ba2b2d8234e0c168f0dbdfb6ec76b6856af709
centvincent <saguinsinvincent@gmail.com>
2025-03-22 04:52:16 +0800

    viewEntity and editEntity routes

--- routes/innerRoutes.php
+++ routes/innerRoutes.php
@@ -73,3 +73,35 @@ Route::get('/editProfile', function () {
 //View and Edit Beneficiary Profile from Table Routing
 Route::post('/viewProfileDetails', [BeneficiaryController::class, 'viewProfileDetails'])->name('viewProfileDetails');
 Route::post('/editProfile', [BeneficiaryController::class, 'editProfile'])->name('editProfile');
+
+Route::get('/viewFamilyDetails', function () {
+    return view('admin.viewFamilyDetails');
+})->name('viewFamilyDetails');
+
+Route::get('/editFamilyProfile', function () {
+    return view('admin.editFamilyProfile');
+})->name('editFamilyProfile');
+
+Route::get('/viewCareworkerDetails', function () {
+    return view('admin.viewCareworkerDetails');
+})->name('viewCareworkerDetails');
+
+Route::get('/editCareworkerProfile', function () {
+    return view('admin.editCareworkerProfile');
+})->name('editCareworkerProfile');
+
+Route::get('/viewCaremanagerDetails', function () {
+    return view('admin.viewCaremanagerDetails');
+})->name('viewCaremanagerDetails');
+
+Route::get('/editCaremanagerProfile', function () {
+    return view('admin.editCaremanagerProfile');
+})->name('editCaremanagerProfile');
+
+Route::get('/viewAdminDetails', function () {
+    return view('admin.viewAdminDetails');
+})->name('viewAdminDetails');
+
+Route::get('/editAdminProfile', function () {
+    return view('admin.editAdminProfile');
+})->name('editAdminProfile');
