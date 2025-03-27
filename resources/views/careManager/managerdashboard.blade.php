fbb94305681909ed3dda66c9afd05e4a05d78cf1
centvincent <saguinsinvincent@gmail.com>
2025-03-27 21:05:05 +0800

    manager, worker dashboard

--- resources/views/careManager/managerdashboard.blade.php
+++ resources/views/careManager/managerdashboard.blade.php
@@ -35,39 +35,93 @@
 
     <div class="home-section">
       <div class="text-left">DASHBOARD CARE MANAGER</div>
-        <div class="container-fluid text-center">
-        <div class="row">
+        <div class="container-fluid">
+            <div class="row boxbox">
               <!-- Statistics Row -->
-              <div class="col-12 col-lg-8" style="background-color: yellow;">
+              <div class="col-12 col-lg-8">
                   <div class="row" id="statistics">
-                      <div class="col-6" id="caregiverStats" style="background-color: green;">
-                          Caregiver Statistics
+                      <div class="col-6" id="caregiverStats" >
+                        <div class="row" style="padding:5px; height: 100%;">
+                            <div class="col-12 caregiverStatsBox" style="background-color: green;">
+                                <h6 class="mt-1">Caregiver Statistics</h6>
+                            </div>
+                        </div>
                       </div>
-                      <div class="col-6" id="beneficiaryStats" style="background-color: blue;">
-                          Beneficiary Statistics
+                      <div class="col-6" id="beneficiaryStats" >
+                        <div class="row" style="padding:5px; height: 100%; padding-left: 0;">
+                            <div class="col-12 beneficiaryStatsBox" style="background-color: blue;">
+                                <h6 class="mt-1">Beneficiary Statistics</h6>
+                            </div>
+                        </div>                          
                       </div>
                   </div>
                   <div class="row" id="recentReports" style="background-color: white;">
-                      <div class="col-12" style="background-color: cyan;">
-                          Recent Reports
+                      <div class="col-12 recentReportBox">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-12">
+                                    <h6 class="mt-1">Recent Reports</h6>
+                                </div>
+                                <div class="col-md-5 col-sm-7 pe-1">
+                                    <form action="" method="GET" id="filterForm">
+                                        <div class="input-group">
+                                            <span class="input-group-text">
+                                                <i class="bx bx-search-alt"></i>
+                                            </span>
+                                            <input type="text" class="form-control" name="search" placeholder="Search report.." id="searchBar" value="{{ request('search') }}">
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="col-md-3 col-sm-5 ps-0">
+                                    <div class="input-group" id="filterGroup">
+                                        <span class="input-group-text">
+                                            <i class="bx bx-filter-alt"></i>
+                                        </span>
+                                        <select class="form-select" name="filter" id="filterDropdown" form="filterForm" onchange="document.getElementById('filterForm').submit();">
+                                            <option value="" selected>Filter by</option>
+                                            <option value="access" {{ request('filter') == 'access' ? 'selected' : '' }}>Access</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered">
+                                            <thead>
+                                                <th style="width: 300px">Author</th>
+                                                <th style="width: 150px">Report Type</th>
+                                                <th style="width: 120px">Date Uploaded</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>FIRST NAME LAST NAME</td>
+                                                    <td>Weekly Careplan</td>
+                                                    <td>03-25-2025</td>
+                                                </tr>
+                                                
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
                       </div>
                   </div>
               </div>
               <!-- Events and Performance Column -->
-              <div class="col-12 col-lg-4 mt-3 mt-lg-0" style="background-color: red;">
+              <div class="col-12 col-lg-4 mt-3 mt-lg-0">
                   <div class="row" id="events">
-                      <div class="col-12" style="background-color: orange;">
-                          Upcoming Events
+                      <div class="col-12 eventsBox" style="background-color: orange;">
+                          <h6 class="mt-1">Upcoming Events</h6>
                       </div>
                   </div>
                   <div class="row" id="performance">
-                      <div class="col-12" style="background-color: pink;">
-                          Caregiver Performance
+                      <div class="col-12 performanceBox" style="background-color: pink;">
+                          <h6 class="mt-1">Caregiver Performance</h6>
                       </div>
                   </div>
               </div>
           </div>
-      </div>
+        </div>
     </div>
    
     <script src=" {{ asset('js/toggleSideBar.js') }}"></script>
