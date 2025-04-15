6795cf5a343d63eda9c01980bbad854d61a4310c
centvincent <saguinsinvincent@gmail.com>
2025-04-15 13:42:57 +0800

    viewProfile admin

--- resources/views/components/viewProfile.blade.php
+++ resources/views/components/viewProfile.blade.php
@@ -11,7 +11,7 @@
 <body>
 
     @include('components.userNavbar')
-    @include('components.sidebar')
+    @include('components.adminSidebar')
     
     <div class="home-section">
         <div class="container-fluid">
